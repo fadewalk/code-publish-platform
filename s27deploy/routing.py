36c8a7b9986ce94a9588280b67be66565962e206
wyule <fadewalk@163.com>
2020-11-30 00:10:08 +0800

    add file
    add file

---
+++ s27deploy/routing.py
@@ -0,0 +1,9 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.conf.urls import url
+from web.views import consumers
+
+application = ProtocolTypeRouter({
+    'websocket': URLRouter([
+        url(r'^publish/(?P<task_id>\d+)/$', consumers.PublishConsumer),
+    ])
+})
